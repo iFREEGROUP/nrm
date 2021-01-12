8f30e75875e57152916f6d925aa63af70c0c366a
Pig Fang <g-plane@hotmail.com>
2021-01-12 18:04:18 +0800

    add CLI

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,44 @@
+use std::{env, path::PathBuf};
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+enum Command {
+    /// Update registry and write to `package-lock.json` file
+    Write(Opt),
+    /// Check if a `package-lock.json` file use specific registry
+    Check(Opt),
+}
+
+#[derive(StructOpt)]
+struct Opt {
+    /// Registry URL
+    #[structopt(long)]
+    registry: String,
+
+    /// Specify path of `package-lock.json`. Default is current directory.
+    #[structopt(long)]
+    path: Option<String>,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
 
-    nrm::update_lockfile("./package-lock.json", "https://registry.npmjs.org").await?;
+    match Command::from_args() {
+        Command::Write(Opt { registry, path }) => {
+            println!("Updating...please wait.");
 
-    Ok(())
+            nrm::update_lockfile(
+                &path.map(PathBuf::from).unwrap_or_else(|| {
+                    let mut cwd =
+                        env::current_dir().expect("failed to retrieve current working directory");
+                    cwd.push("package-lock.json");
+                    cwd
+                }),
+                registry.trim_end_matches("/"),
+            )
+            .await
+        }
+        Command::Check(_) => unimplemented!(),
+    }
 }
